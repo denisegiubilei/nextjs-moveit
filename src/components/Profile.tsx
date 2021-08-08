5742c0bb4d21ac21b043467a4fed8695be8811c9
Denise Giubilei <giubileidenise@gmail.com>
2021-08-08 19:29:27 -0300

    fix use img html tag

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,19 +1,17 @@
-import Image from 'next/image';
-
 import styles from '../styles/components/Profile.module.css';
 
 function Profile() {
   return (
     <div className={styles.profileContainer}>
-      <Image
-        src={'http://github.com/denisegiubilei.png' as any}
+      <img
+        src="http://github.com/denisegiubilei.png"
         alt="Foto de perfil do usuário denisegiubilei"
       />
       <div>
         <strong>Denise Giubilei</strong>
         <p>
-          <Image
-            src={'icons/level.svg' as any}
+          <img
+            src="icons/level.svg"
             alt="Ícone de level - Seta para cima"
           />
           Level 1
