96d70156bc482ad87a56b39edaf506351d668112
Denise Giubilei <giubileidenise@gmail.com>
2021-08-16 20:43:26 -0300

    feat: play notification when start new challenge

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -46,7 +46,16 @@ export function ChallengesContextProvider({
   function startNewChallenge() {
     const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
     const challenge = challenges[randomChallengeIndex];
+
     setActiveChallenge(challenge);
+
+    new Audio('./notification.mp3').play();
+
+    if (Notification.permission === 'granted') {
+      new Notification('Novo desafio! 🎉', {
+        body: `Valendo ${challenge.amount}xp!`
+      });
+    }
   }
 
   function resetActiveChallenge() {
